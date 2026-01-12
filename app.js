9e78a4369857708ddb42979c38f820b0ae041572
ChandanaBN3 <chandanamonikha@gmail.com>
2026-01-12 19:22:13 +0530

    Fix flash message locals

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const mongo_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+// const mongo_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 const dbUrl = process.env.ATLASDB_URL;
+console.log("ATLASDB_URL =", dbUrl);
 
 main()
     .then(() => {
